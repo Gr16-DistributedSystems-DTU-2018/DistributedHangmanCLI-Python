0645564efe520e233e45786c1dcf0dbb21db56ab
InAbsentia <absentia@protonmail.ch>
2018-04-12 17:55:52 +0200

    REST API WIP

--- src/rest.py
+++ src/rest.py
@@ -4,7 +4,7 @@ import json
 url = 'http://ubuntu4.saluton.dk:8080/web/rest/game'
 
 def login(username, password) -> bool:
-	local_url = url + "/login?username={}&password={}".format(username, password)
+	local_url = "{}/login?username={}&password={}".format(url, username, password)
 	result = requests.post(local_url)
 	if is_ok_status(result):
 		if result.text == username + " logged in successfully.":
@@ -15,7 +15,7 @@ def login(username, password) -> bool:
 		return False
 
 def logout(username) -> bool:
-	local_url = url + "/logout?username={}".format(username)
+	local_url = "{}/logout?username={}".format(url, username)
 	result = requests.post(local_url)
 	if is_ok_status(result):
 		if result.text == username + " logged out successfully.":
@@ -25,26 +25,53 @@ def logout(username) -> bool:
 	else:
 		return False
 
+# NOT WORKING
 def get_all_current_usernames() -> list:
-	pass
+	local_url = "{}/get_all_current_usernames".format(url)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		return result.text;
+	else:
+		return list
 
 def get_current_user_amount() -> int:
-	pass
+	local_url = "{}/get_current_user_amount".format(url)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		return result.text;
+	else:
+		return -1
 
 #def get_logged_in_user() -> 
 #	pass
 
 def is_logged_in(username) -> bool:
-	pass
+	local_url = "{}/is_logged_in?username={}".format(url, username)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		return result.text == "true"
+	else:
+		return False
 
 #def get_user_with_highest_highscore() -> :
 #	pass
 
 def set_user_highscore(username, highscore) -> bool:
-	pass
+	local_url = "{}/set_user_highscore?username={}&highscore={}".format(url, username, highscore)
+	result = requests.post(local_url)
+	if is_ok_status(result):
+		if (result.text == "{}: set highscore: {}".format(username, highscore)):
+			return True
+	else:
+		return False
 
 def get_user_highscore(username) -> int:
-	pass
+	local_url = "{}/get_user_highscore?username={}".format(url, username)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		return int(result.text)
+	else:
+		return -1
 
 #def get_all_logged_in_users_score() -> 
 #	pass
@@ -53,13 +80,36 @@ def get_user_highscore(username) -> int:
 #	pass
 
 def send_email(username, password, subject, msg) -> bool:
-	pass
+	msg += "\n\nSendt via Gruppe 16 - DistributedHangman - Python Klient"
+	local_url = "{}/send_email?username={}&password={}&subject={}&msg={}".format(url, username, password, subject, msg)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		if (result.text == "Sent email to {}. Subject: {}: msg: {}".format(username, subject, msg)):
+			return True
+		else:
+			return False
+	else:
+		return False
 
 def send_forgot_password_email(username, msg) -> bool:
-	pass
+	msg += "\n\nSendt via Gruppe 16 - DistributedHangman - Python Klient"
+	local_url = "{}/send_forgot_password_email?username={}&msg={}".format(url, username, msg)
+	result = requests.get(local_url)
+	if is_ok_status(result):
+		return True
+	else:
+		return False
 
 def change_user_password(username, oldPassword, newPassword):
-	pass
+	local_url = "{}/change_user_password?username={}&oldPassword={}&newPassword={}".format(url, username, oldPassword, newPassword)
+	result = requests.post(local_url)
+	if is_ok_status(result):
+		if (result.text == "Changed password for: {}".format(username)):
+			return True
+		else:
+			return False
+	else:
+		return False
 
 def guess(char) -> bool:
 	pass
@@ -128,4 +178,4 @@ def is_ok_status(result) -> bool:
 	else:
 		return False
 
-print(login("jacno", "xxx"))
+print(change_user_password("s151641", "hejsa", "godkode"))
